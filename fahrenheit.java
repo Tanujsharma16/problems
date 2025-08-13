907882ab37f7db8908cc53bff6fd5aeb3acbdccc
Tanujsharma16 <tanujpandit16q@gmail.com>
2025-08-13 10:49:31 +0530

    Update fahrenheit.java

--- fahrenheit.java
+++ fahrenheit.java
@@ -1,7 +1,7 @@
 public class fahrenheit{
-//     public static void main(String[] args) {
-//         Scanner sc=new Scanner(System.in);
-//         int celsius= sc.nextInt();
-//         System.out.println("Fahrenheit is: " + (celsius * 9/5 + 32));
-//     }
-//     }
+     public static void main(String[] args) {       
+       Scanner sc=new Scanner(System.in);
+         int celsius= sc.nextInt();
+         System.out.println("Fahrenheit is: " + (celsius * 9/5 + 32));     
+     }
+     }
