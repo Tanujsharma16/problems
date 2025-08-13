7703dc10a1e08cadc1c0ae30ba043da52ea64100
Tanujsharma16 <tanujpandit16q@gmail.com>
2025-08-13 10:55:02 +0530

    Create miles.java

---
+++ miles.java
@@ -0,0 +1,7 @@
+public class miles{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int kilometers= sc.nextInt();
+        System.out.println("Miles is: " + (kilometers * 0.621371));
+    }
+    }
