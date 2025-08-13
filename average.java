8ef234c5bca386b73636e21b8ce76fa47e7827f0
Tanujsharma16 <tanujpandit16q@gmail.com>
2025-08-13 10:54:17 +0530

    Create average.java

---
+++ average.java
@@ -0,0 +1,9 @@
+public class average{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int first= sc.nextInt();
+        int second= sc.nextInt();
+        int third= sc.nextInt();
+       System.out.println("Average is: " + ((first + second + third) / 3.0));
+    }
+    }
