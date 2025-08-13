aae0b16228fb96772c3e481cc4d2c2e9d8eb8336
Tanujsharma16 <tanujpandit16q@gmail.com>
2025-08-13 10:52:53 +0530

    Create rectangle.java

---
+++ rectangle.java
@@ -0,0 +1,8 @@
+public class rectangle{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int length= sc.nextInt();
+        int width= sc.nextInt();
+        System.out.println("Area of Rectangle is: " + (length * width));
+    }
+    
