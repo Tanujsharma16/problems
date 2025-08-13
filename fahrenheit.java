e9ff1b8dd3d823383b70ad5f368394e41b3b625a
Tanujsharma16 <tanujpandit16q@gmail.com>
2025-08-13 10:47:45 +0530

    Create fahrenheit.java

---
+++ fahrenheit.java
@@ -0,0 +1,7 @@
+public class fahrenheit{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int celsius= sc.nextInt();
+//         System.out.println("Fahrenheit is: " + (celsius * 9/5 + 32));
+//     }
+//     }
