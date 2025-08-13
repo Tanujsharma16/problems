7edf13d5d16b16d2c585339b69f5914c2d33cea1
Tanujsharma16 <tanujpandit16q@gmail.com>
2025-08-13 10:48:50 +0530

    Create circle.java

---
+++ circle.java
@@ -0,0 +1,7 @@
+public class recuraion{
+     public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+         int radius= sc.nextInt();
+        System.out.println("Area of Circle is: " + (Math.PI * radius * radius));
+    }
+    }
