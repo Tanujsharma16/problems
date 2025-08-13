c2838412d28a3dcd01a2b55e1a2910e8d0de692f
Tanujsharma16 <tanujpandit16q@gmail.com>
2025-08-13 10:53:34 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,8 @@
+public class power{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int base= sc.nextInt();
+        int exponent= sc.nextInt();
+        System.out.println("Power is: " + Math.pow(base, exponent));
+    }
+    }
