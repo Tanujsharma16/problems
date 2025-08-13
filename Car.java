d6fde9031be15ca5009469501ab152879a141bfe
Tanujsharma16 <tanujpandit16q@gmail.com>
2025-08-13 11:09:25 +0530

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,8 @@
+public class Car {
+   String brand;
+   int speed;
+ public static void main(String[] args) {
+    int wheelCount = 4; 
+    System.out.println("Number of wheels: " + wheelCount);
+}
+}
